bd88c4e6cf3e0c34aeeeacb09bbe6903386dffba
José L. Gutiérrez <gutiecuador@gmail.com>
2025-06-05 17:24:14 -0500

    [FIX] Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SysCom API - Excel Generator</title>
+        <title>MSP APP - WooCommerce Integration</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
