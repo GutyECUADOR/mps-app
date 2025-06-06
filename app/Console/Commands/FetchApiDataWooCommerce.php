67ddfa90df83b22f8835f985b05da6a87933ca80
José L. Gutiérrez <gutiecuador@gmail.com>
2025-06-06 11:19:03 -0500

    [FEAT] Implement FetchApiDataWooCommerce command and ApiWooCommerceService for WooCommerce data retrieval

---
+++ app/Console/Commands/FetchApiDataWooCommerce.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\ApiWooCommerceService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class FetchApiDataWooCommerce extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:fetch-api-data-woo-commerce';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Consulta las ordernes de WooCommerce desde la API externa y las almacena en la base de datos local.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ApiWooCommerceService $apiWooCommerceService): void
+    {
+        $this->info('Iniciando la consulta de datos de WooCommerce...');
+
+        // Llamamos al servicio para obtener los datos
+        $data = $apiWooCommerceService->fetchExternalData();
+        Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/ApiWooCommerceService.log'),
+            ])->info(json_encode($data));
+
+        if ($data) {
+            $this->info('Datos obtenidos exitosamente.');
+            // Aquí podrías procesar y almacenar los datos en la base de datos local
+        } else {
+            $this->error('No se pudieron obtener los datos de WooCommerce o no existen nuevas ordenes.');
+        }
+
+        $this->info('Consulta finalizada.');
+    }
+}
---
+++ app/Services/ApiWooCommerceService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
+
+class ApiWooCommerceService
+{
+    protected $client;
+
+    public function __construct()
+    {
+        // Inicializamos GuzzleHttp\Client
+        $this->client = new Client([
+            'base_uri' => env('BASE_URL_API_WOOCOMMERCE'),
+            'timeout'  => 5.0,
+        ]);
+    }
+
+    public function fetchExternalData(): ?array
+    {
+        try {
+            $currentDate = now()->subMonth()->format('Y-m-d\T00:00:00-05:00');
+            Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/ApiWooCommerceService.log'),
+            ])->info('Ordenes de WooCommerce hasta la fecha:'.$currentDate);
+
+            $response = $this->client->get("/wp-json/wc/v3/orders?after={$currentDate}", [
+                'auth' => [
+                    env('APP_KEY_API_WOOCOMMERCE'),
+                    env('APP_SECRET_API_WOOCOMMERCE')
+                ]
+            ]);
+
+            if ($response->getStatusCode() === 200) {
+                return json_decode($response->getBody()->getContents(), true);
+            }
+
+            return null;
+        } catch (RequestException $e) {
+            // Registramos el error para poder depurarlo
+            Log::error('Error al obtener datos de la API externa: ' . $e->getMessage());
+            Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/errors-ApiWooCommerceService.log'),
+            ])->error('Error al obtener datos de la API externa BUILD: '. $e->getMessage());
+            return null;
+        }
+    }
+}
