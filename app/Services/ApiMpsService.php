e6a661fd5b1e150345662ba89700df885887913d
José L. Gutiérrez <gutiecuador@gmail.com>
2025-08-10 20:08:37 -0500

    [FIX] Send single Pedido by request

--- app/Services/ApiMpsService.php
+++ app/Services/ApiMpsService.php
@@ -28,69 +28,10 @@ class ApiMpsService
             ->whereNull('processing_mps_date')
             ->get();
 
-        // Mapeo de WooCommerceOrder a Pedido
-        $listaPedidos = $wc_orders->map(function ($order) {
-            $listaPedidoDetalle = $order->lineItems->map(function ($item) {
-
-                // Consulta a API de WooCommerce para obtener detalles del producto
-                $productDetails = null;
-                try {
-                    $wcClient = new Client([
-                        'base_uri' => env('BASE_URL_API_WOOCOMMERCE'),
-                        'timeout'  => 5.0,
-                        'auth'     => [env('APP_KEY_API_WOOCOMMERCE'), env('APP_SECRET_API_WOOCOMMERCE')],
-                    ]);
-                    $response = $wcClient->get("/wp-json/wc/v3/products/{$item->product_id}");
-                    $productDetails = json_decode($response->getBody(), true);
-
-                    Log::build([
-                        'driver' => 'single',
-                        'path' => storage_path('logs/ApiWooCommerceService.log'),
-                    ])->info('Marca producto consultado: ' . json_encode($productDetails['brands'][0]['name'] ?? 'No disponible'));
-                } catch (\Exception $e) {
-                    Log::error('Error al consultar producto WooCommerce: ' . $e->getMessage());
-                }
-
-                return [
-                    'PartNum'  => $item->sku,
-                    'Cantidad' => $item->quantity,
-                    // Asegúrate de tener un valor por defecto si la marca no existe para evitar errores
-                    'Marks'    => $productDetails['brands'][0]['name'] ?? 'N/A',
-                    'Bodega'   => 'BCOTA',
-                ];
-            })->toArray();
-
-            // Devuelve la estructura de un único pedido
-            return [
-                'AccountNum'           => '79580718', // Numero de Cuenta
-                'NombreClienteEntrega' => $order->billingAddress->first_name . ' ' . $order->billingAddress->last_name,
-                'ClienteEntrega'       => '79580718', // Numero de DNI
-                'TelefonoEntrega'      => $order->billingAddress->phone,
-                'DireccionEntrega'     => $order->billingAddress->address_1 . ' ' . $order->billingAddress->address_2,
-                'StateId'              => $order->billingAddress->state,
-                'CountyId'             => $order->billingAddress->city,
-                'RecogerEnSitio'       => 0,
-                'EntregaUsuarioFinal'  => 0,
-                'dlvTerm'              => '',
-                'dlvmode'              => '',
-                'Observaciones'        => 'Pedido Web Nro. ' . $order->woocommerce_id,
-                'listaPedidoDetalle'   => $listaPedidoDetalle // El detalle ahora está anidado aquí
-            ];
-        });
-
-        // Construye el objeto final en el formato deseado
-        $objectResponse = [
-            'listaPedido' => $listaPedidos->toArray()
-        ];
-
-        // Log de pedidos
-        Log::build([
-            'driver' => 'single',
-            'path' => storage_path('logs/ApiWooCommerceService.log'),
-        ])->info('Pedidos enviados a MPS: ' . json_encode($objectResponse));
+        $API_responses = [];
 
         try {
-
+            // Obtener token solo una vez
             $response = $this->client->post('/Token', [
                 'headers' => [
                     'Content-Type' => 'application/x-www-form-urlencoded',
@@ -103,33 +44,94 @@ class ApiMpsService
                 ],
             ]);
 
-            // Obtener el cuerpo de la respuesta
             $body = $response->getBody();
             $result = json_decode($body, true);
-            $token = $result['access_token']; // Get Access Token
+            $token = $result['access_token'];
 
-            $response = $this->client->post('/api/WebApi/RealizarPedido', [
-                'headers' => [
-                    'Authorization' => 'Bearer ' . $token, // Set the Authorization header
-                    'Accept'        => 'application/json',
-                    'Content-Type'  => 'application/json',
-                ],
-                'json' => $objectResponse, // Envía el objeto JSON correctamente estructurado
-            ]);
+            foreach ($wc_orders as $order) {
+                $listaPedidoDetalle = $order->lineItems->map(function ($item) {
+                    $productDetails = null;
+                    try {
+                        $wcClient = new Client([
+                            'base_uri' => env('BASE_URL_API_WOOCOMMERCE'),
+                            'timeout'  => 5.0,
+                            'auth'     => [env('APP_KEY_API_WOOCOMMERCE'), env('APP_SECRET_API_WOOCOMMERCE')],
+                        ]);
+                        $response = $wcClient->get("/wp-json/wc/v3/products/{$item->product_id}");
+                        $productDetails = json_decode($response->getBody(), true);
 
-            // Get productos
-            $body = $response->getBody();
-            $response = json_decode($body, true);
+                        Log::build([
+                            'driver' => 'single',
+                            'path' => storage_path('logs/ApiWooCommerceService.log'),
+                        ])->info('Marca producto consultado: ' . json_encode($productDetails['brands'][0]['name'] ?? 'No disponible'));
+                    } catch (\Exception $e) {
+                        Log::error('Error al consultar producto WooCommerce: ' . $e->getMessage());
+                    }
 
-            return $response;
+                    return [
+                        'PartNum'  => $item->sku,
+                        'Cantidad' => $item->quantity,
+                        'Marks'    => $productDetails['brands'][0]['name'] ?? 'N/A',
+                        'Bodega'   => 'BCOTA',
+                    ];
+                })->toArray();
+
+                $pedido = [
+                    'AccountNum'           => '79580718',
+                    'NombreClienteEntrega' => $order->billingAddress->first_name . ' ' . $order->billingAddress->last_name,
+                    'ClienteEntrega'       => '79580718',
+                    'TelefonoEntrega'      => $order->billingAddress->phone,
+                    'DireccionEntrega'     => $order->billingAddress->address_1 . ' ' . $order->billingAddress->address_2,
+                    'StateId'              => $order->billingAddress->state,
+                    'CountyId'             => $order->billingAddress->city,
+                    'RecogerEnSitio'       => 0,
+                    'EntregaUsuarioFinal'  => 0,
+                    'dlvTerm'              => '',
+                    'dlvmode'              => '',
+                    'Observaciones'        => 'Pedido Web Nro. ' . $order->woocommerce_id,
+                    'listaPedidoDetalle'   => $listaPedidoDetalle
+                ];
+
+                $objectRequest = [
+                    'listaPedido' => [$pedido]
+                ];
+
+                Log::build([
+                    'driver' => 'single',
+                    'path' => storage_path('logs/ApiWooCommerceService.log'),
+                ])->info('Pedido enviado a MPS: ' . json_encode($objectRequest));
+
+                try {
+                    $response = $this->client->post('/api/WebApi/RealizarPedido', [
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $token,
+                            'Accept'        => 'application/json',
+                            'Content-Type'  => 'application/json',
+                        ],
+                        'json' => $objectRequest,
+                    ]);
+
+                    $body = $response->getBody();
+                    $apiResponse = json_decode($body, true);
+                    $API_responses[] = $apiResponse;
+                } catch (RequestException $e) {
+                    Log::error('Error al enviar pedido a MPS: ' . $e->getMessage());
+                    Log::build([
+                        'driver' => 'single',
+                        'path' => storage_path('logs/errors-ApiWooCommerceService.log'),
+                    ])->error('Error al enviar pedido a MPS BUILD: ' . $e->getMessage());
+                    $API_responses[] = ['error' => $e->getMessage()];
+                }
+            }
         } catch (RequestException $e) {
-            // Registramos el error para poder depurarlo
-            Log::error('Error al obtener datos de la API externa: ' . $e->getMessage());
+            Log::error('Error al obtener token de la API externa: ' . $e->getMessage());
             Log::build([
                 'driver' => 'single',
                 'path' => storage_path('logs/errors-ApiWooCommerceService.log'),
-            ])->error('Error al obtener datos de la API externa BUILD: ' . $e->getMessage());
+            ])->error('Error al obtener token de la API externa BUILD: ' . $e->getMessage());
             return null;
         }
+
+        return $API_responses;
     }
 }
